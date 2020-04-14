30ba94dabf67c51c092bac6bebf37efc59226c88
qinwh <1510365643@qq.com>
2020-04-15 05:21:58 +0800

    create reply

---
+++ src/main/java/cn/qinwh/reply/ReplyApplication.java
@@ -0,0 +1,13 @@
+package cn.qinwh.reply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/qinwh/reply/controller/UserController.java
@@ -0,0 +1,24 @@
+package cn.qinwh.reply.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    /**
+    * @Description: 
+    * @Param: [name]
+    * @return: java.lang.String
+    * @Author: qinwh
+    * @Date: 2020/4/15
+    */
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(String name){
+        return "hello " + name;
+    }
+
+}
