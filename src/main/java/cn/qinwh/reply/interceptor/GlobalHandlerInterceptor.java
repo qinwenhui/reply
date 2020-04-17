3c434fa6ce26dfca6ce635fce0f6665d5a57dcfe
qinwh <1510365643@qq.com>
2020-04-17 17:41:41 +0800

    设置跨域时，自定义请求header参数需要设置到response的header跨域设置里面

--- src/main/java/cn/qinwh/reply/interceptor/GlobalHandlerInterceptor.java
+++ src/main/java/cn/qinwh/reply/interceptor/GlobalHandlerInterceptor.java
@@ -16,9 +16,10 @@ public class GlobalHandlerInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		//执行handler之前,在这里进行拦截，设置响应跨域
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type,Token");
 		response.setContentType("application/json; charset=utf-8");
 		System.out.println("跨域设置拦截成功");
 		return true;
